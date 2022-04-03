c512d2181bd9aa55087a44807fa3693837bdce5e
Ruslan Khamzin <hamzin362@gmail.com>
2022-04-03 20:13:47 +0400

    0

--- src/main/java/com/example/buysell/controllers/UserController.java
+++ src/main/java/com/example/buysell/controllers/UserController.java
@@ -3,9 +3,12 @@ package com.example.buysell.controllers;
 import com.example.buysell.models.User;
 import com.example.buysell.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.dom4j.rule.Mode;
 import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -30,8 +33,12 @@ public class UserController {
         return "redirect:/login";
     }
 
-    @GetMapping("/hello")
-    public String securityUrl() {
-        return "hello";
+
+    @GetMapping("/user/{user}")
+    public String userinfo(@PathVariable User user, Model model){
+        model.addAttribute("user",user);
+        model.addAttribute("products",user.getProducts());
+        return "user-info";
+
     }
 }

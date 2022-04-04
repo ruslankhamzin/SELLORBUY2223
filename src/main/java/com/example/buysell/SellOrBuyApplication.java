a6ee38ef1db22688db3f53fa368037098de3f2b9
Ruslan Khamzin <hamzin362@gmail.com>
2022-04-04 19:57:16 +0400

    0

--- src/main/java/com/example/buysell/SellOrBuyApplication.java
+++ src/main/java/com/example/buysell/SellOrBuyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BuysellApplication {
+public class SellOrBuyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BuysellApplication.class, args);
+		SpringApplication.run(SellOrBuyApplication.class, args);
 	}
 
 }
